cbc821fe265b505d93b7bedbd4a22d3e5d475831
Omer Cohen <omercohen280105@gmail.com>
2022-03-30 17:46:20 +0300

    changes to Thread class
    added some attributes to thread, still needs methods and a way for datetime (look at comments), Benny is annoying

--- thread.py
+++ thread.py
@@ -1,6 +1,12 @@
 import time
 class Thread:
 
-    def __init__(self,creator:str, topcis:list[str]):
+    def __init__(self,creator:str, topcis:list[str],room_id:int, banned_words:list[str]):
         self.creator = creator
         self.topcis = topcis
+        self.admins_list = []
+        self.members = []
+        #self.create_time #add a way to know when it was created
+        self.room_id = room_id
+        self.common_words = []
+        self.banned_words = banned_words
